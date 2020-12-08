eeed47a6bf7b1c1e39751240ae67fada709dcb8d
Lorenzo Arena <arena.lor@gmail.com>
2020-12-08 19:23:07 +0100

    Added first complete implementation, not working correctly

--- src/sequential/main.c
+++ src/sequential/main.c
@@ -5,6 +5,8 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 #include "hsl.h"
 #include "log.h"
@@ -20,6 +22,18 @@ void histogram_calc(unsigned int *hist, float *lum, size_t img_size)
     for(unsigned int index = 0; index < img_size; index++)
     {
         hist[(int)round(lum[index] * N_BINS)]++;
+    }
+}
+
+void cdf_calc(unsigned int *cdf, unsigned int *buf, size_t buf_size)
+{
+    assert(cdf != NULL);
+    assert(buf != NULL);
+
+    cdf[0] = buf[0];
+    for(unsigned int index = 1; index < buf_size; index++)
+    {
+        cdf[index] = cdf[index - 1] + buf[index];
     }
 }
 
@@ -81,24 +95,78 @@ int main(int argc, char **argv)
     log_info("Starting histogram calculation..");
     histogram_calc(histogram, hsl_image.l, width * height);
 
-    stbi_image_free(rgb_image);
+    // Important: since it can be that a lot of pixel are black or white,
+    // all calculations on the histogram must exclude the extremes
 
-    stopwatch_stop();
+    // Calculate the cdf
+    unsigned int *cdf = calloc(N_BINS - 1, sizeof(unsigned int));
+    log_info("Starting cdf calculation..");
+    cdf_calc(cdf, &histogram[1], N_BINS - 1);
 
-    struct timespec elapsed = stopwatch_get_elapsed();
+    // Normalize the cdf so that it can be used as luminance
+    float *cdf_norm = calloc(N_BINS - 1, sizeof(float));
+    log_info("Starting normalized cdf calculation..");
+    for(int bin = 0; bin < (N_BINS - 2); bin++)
+    {
+        float temp = (float)(cdf[bin] - cdf[0]);
+        float temp2 = (float)(cdf[N_BINS - 2] - cdf[0]);
+        float temp3 = (float)(N_BINS - 1);
+        cdf_norm[bin] = (float)(temp) / temp2;
+        //cdf_norm[bin] = (unsigned int)round(((float)(cdf[bin] - cdf[0]) / (float)(cdf[N_BINS - 2] - cdf[0])) * (float)(N_BINS - 1));
+    }
 
-    log_info("Elapsed time: %ld.%09ld",
-        elapsed.tv_sec,
-        elapsed.tv_nsec);
+    // Apply the normalized cdf to the luminance
+    for(int x = 0; x < width; x++)
+    {
+        for(int y = 0; y < height; y++)
+        {
+            hsl_image.l[(width * x) + y] = cdf_norm[(int)round(hsl_image.l[(width * x) + y] * N_BINS)];
+        }
+    }
 
-    log_info("Printing hystogram..");
+    // Convert back to rgb and save the image
+    for(int x = 0; x < width; x++)
+    {
+        for(int y = 0; y < height; y++)
+        {
+            unsigned char* pixel_offset = rgb_image + (((width * x) + y) * STBI_rgb);
+
+            rgb_pixel_t rgb_pixel = { .r = 0, .g = 0, .b = 0 };
+
+            hsl_pixel_t hsl_pixel = {
+                .h = hsl_image.h[(width * x) + y],
+                .s = hsl_image.s[(width * x) + y],
+                .l = hsl_image.l[(width * x) + y]
+            };
+
+            hsl_to_rgb(hsl_pixel, &rgb_pixel);
+
+            pixel_offset[0] = rgb_pixel.r;
+            pixel_offset[1] = rgb_pixel.g;
+            pixel_offset[2] = rgb_pixel.b;
+        }
+    }
+
+    stbi_write_jpg("test.jpg", width, height, STBI_rgb, rgb_image, 100);
+
+    // TODO: add an option for print
+    log_info("Printing histogram..");
     for(int bin = 0; bin < N_BINS; bin++)
     {
         log_info("%d:%d", bin, histogram[bin]);
     }
 
-    // Important: since it can be that a lot of pixel are black or white,
-    // all calculations on the histogram must exclude the extremes
+    log_info("Printing cdf..");
+    for(int bin = 0; bin < (N_BINS - 1); bin++)
+    {
+        log_info("%d:%d", bin, cdf[bin]);
+    }
+
+    log_info("Printing normalized cdf..");
+    for(int bin = 0; bin < (N_BINS - 1); bin++)
+    {
+        log_info("%d:%g", bin, cdf_norm[bin]);
+    }
 
     // TODO: add an option for plot
     FILE *gnuplot = popen("gnuplot -persistent", "w");
@@ -110,6 +178,22 @@ int main(int argc, char **argv)
     fprintf(gnuplot, "e\n");
     fprintf(gnuplot,"set xrange[1:%d]\n", N_BINS);
     fflush(gnuplot);
+
+    // Clean up buffers
+    stbi_image_free(rgb_image);
+
+    free(hsl_image.h);
+    free(hsl_image.s);
+    free(hsl_image.l);
+    free(histogram);
+
+    stopwatch_stop();
+
+    struct timespec elapsed = stopwatch_get_elapsed();
+
+    log_info("Elapsed time: %ld.%09ld",
+        elapsed.tv_sec,
+        elapsed.tv_nsec);
 
     return 0;
 }
