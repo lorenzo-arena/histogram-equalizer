c7733a22b45729bfb7cc97fc9eb35704809c83d2
Lorenzo Arena <arena.lor@gmail.com>
2020-12-14 19:36:46 +0100

    Better management of argp variables

--- src/common/arguments.h
+++ src/common/arguments.h
@@ -18,19 +18,9 @@ extern const char *argp_program_version;
 
 extern const char doc[];
 
+extern struct argp argp;
+
 void set_default_arguments(struct arguments *arguments);
 error_t parse_opt(int key, char *arg, struct argp_state *state);
-
-static char args_doc[] = "image output";
-
-static struct argp_option options[] =
-{
-    {"stopwatch", 's', 0, 0, "Enable stopwatch usage", 0},
-    {"plot", 'p', 0, 0, "Enable histogram plot", 0},
-    {"log_histogram", 'l', 0, 0, "Enable histogram log", 0},
-    {0}
-};
-
-static struct argp argp = {options, parse_opt, args_doc, doc, NULL, NULL, NULL};
 
 #endif
